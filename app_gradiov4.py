1ae15767c16c68cc03962660dab721639073c04e
catoralonso <157717738+catoralonso@users.noreply.github.com>
2026-03-04 13:58:23 +0100

    Update app_gradiov4.py

--- app_gradiov4.py
+++ app_gradiov4.py
@@ -858,6 +858,7 @@ with gr.Blocks(title="🧊 EatguAI 🧊", theme=gr.themes.Base(), css=CSS_CUSTOM
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
     demo.launch(
         server_name="0.0.0.0",
         server_port=port,
